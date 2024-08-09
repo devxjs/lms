06498d1ee36a48fa6e9bc7693589f98b823bc5c8
devxjs <117807058+devxjs@users.noreply.github.com>
2024-08-09 22:39:43 +0500

    Update index.js

--- frontend/src/utils/index.js
+++ frontend/src/utils/index.js
@@ -2,8 +2,23 @@ import { toast } from 'frappe-ui'
 import { useTimeAgo } from '@vueuse/core'
 import { Quiz } from '@/utils/quiz'
 import { Upload } from '@/utils/upload'
-import Header from '@editorjs/header'
-import Paragraph from '@editorjs/paragraph'
+import EditorJS from '@editorjs/editorjs'
+//import Header from '@editorjs/header'
+//const Header = require("editorjs-header-with-alignment");
+import Header from 'editorjs-header-with-alignment'
+//import Paragraph from '@editorjs/paragraph'
+//const Paragraph = require('editorjs-paragraph-with-alignment');
+import Paragraph from 'editorjs-paragraph-with-alignment'
+import Table from '@editorjs/table';
+//const ColorPlugin = require('editorjs-text-color-plugin');
+import ColorPlugin from 'editorjs-text-color-plugin'
+import Underline from '@editorjs/underline';
+import Hyperlink from 'editorjs-hyperlink';
+import Title from "title-editorjs";
+import Alert from 'editorjs-alert';
+import Delimiter from '@editorjs/delimiter';
+import editorjsColumns from '@calumk/editorjs-columns';
+
 import { CodeBox } from '@/utils/code'
 import NestedList from '@editorjs/nested-list'
 import InlineCode from '@editorjs/inline-code'
@@ -133,8 +148,99 @@ export function htmlToText(html) {
 }
 
 export function getEditorTools() {
+
+	let column_tools = {
+		header: {
+			class: Header,
+			//inlineToolbar: ["link", "marker", "header", "color","left", "center", "right"],
+			config: {
+			  placeholder: 'Enter a header',
+			  levels: [1,2, 3, 4,5,6],
+			  defaultLevel: 4,
+			  defaultAlignment: 'left'
+			}
+		  },
+		  Color: {
+			class: ColorPlugin, // if load from CDN, please try: window.ColorPlugin
+			config: {
+			   colorCollections: ['#EC7878','#9C27B0','#673AB7','#3F51B5','#0070FF','#03A9F4','#00BCD4','#4CAF50','#8BC34A','#CDDC39', '#FFF'],
+			   defaultColor: '#FF1300',
+			   type: 'text', 
+			   customPicker: true // add a button to allow selecting any colour  
+			}     
+		  },
+		  Marker: {
+			class: ColorPlugin, // if load from CDN, please try: window.ColorPlugin
+			config: {
+			   defaultColor: '#FFBF00',
+			   type: 'marker',
+			   icon: `<svg fill="#000000" height="200px" width="200px" version="1.1" id="Icons" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" viewBox="0 0 32 32" xml:space="preserve"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"> <g> <path d="M17.6,6L6.9,16.7c-0.2,0.2-0.3,0.4-0.3,0.6L6,23.9c0,0.3,0.1,0.6,0.3,0.8C6.5,24.9,6.7,25,7,25c0,0,0.1,0,0.1,0l6.6-0.6 c0.2,0,0.5-0.1,0.6-0.3L25,13.4L17.6,6z"></path> <path d="M26.4,12l1.4-1.4c1.2-1.2,1.1-3.1-0.1-4.3l-3-3c-0.6-0.6-1.3-0.9-2.2-0.9c-0.8,0-1.6,0.3-2.2,0.9L19,4.6L26.4,12z"></path> </g> <g> <path d="M28,29H4c-0.6,0-1-0.4-1-1s0.4-1,1-1h24c0.6,0,1,0.4,1,1S28.6,29,28,29z"></path> </g> </g></svg>`
+			  }       
+		  },
+		alert : Alert,
+		paragraph: {
+			class: Paragraph,
+			inlineToolbar: true,
+			config: {
+				preserveBlank: true,
+			},
+		},
+		delimiter : Delimiter,
+		underline : Underline,
+		hyperlink : Hyperlink,
+		title : Title,
+		image: SimpleImage,
+	};
+
+
 	return {
-		header: Header,
+		header: {
+			class: Header,
+			//inlineToolbar: ["link", "marker", "header", "color","left", "center", "right"],
+			config: {
+			  placeholder: 'Enter a header',
+			  levels: [1,2, 3, 4,5,6],
+			  defaultLevel: 4,
+			  defaultAlignment: 'left'
+			}
+		  },	
+		  alert : Alert,
+		  delimiter : Delimiter,
+		  underline : Underline,
+		  hyperlink : Hyperlink,
+		  title : Title,
+		 // columns : {
+		//	class : editorjsColumns,
+		//	config : {
+		//	  EditorJsLibrary : EditorJs, // Pass the library instance to the columns instance.
+		//	  tools : column_tools // IMPORTANT! ref the column_tools
+		//	}
+		  //},
+		  Color: {
+			class: ColorPlugin, // if load from CDN, please try: window.ColorPlugin
+			config: {
+			   colorCollections: ['#EC7878','#9C27B0','#673AB7','#3F51B5','#0070FF','#03A9F4','#00BCD4','#4CAF50','#8BC34A','#CDDC39', '#FFF'],
+			   defaultColor: '#FF1300',
+			   type: 'text', 
+			   customPicker: true // add a button to allow selecting any colour  
+			}     
+		  },
+		  Marker: {
+			class: ColorPlugin, // if load from CDN, please try: window.ColorPlugin
+			config: {
+			   defaultColor: '#FFBF00',
+			   type: 'marker',
+			   icon: `<svg fill="#000000" height="200px" width="200px" version="1.1" id="Icons" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" viewBox="0 0 32 32" xml:space="preserve"><g id="SVGRepo_bgCarrier" stroke-width="0"></g><g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g><g id="SVGRepo_iconCarrier"> <g> <path d="M17.6,6L6.9,16.7c-0.2,0.2-0.3,0.4-0.3,0.6L6,23.9c0,0.3,0.1,0.6,0.3,0.8C6.5,24.9,6.7,25,7,25c0,0,0.1,0,0.1,0l6.6-0.6 c0.2,0,0.5-0.1,0.6-0.3L25,13.4L17.6,6z"></path> <path d="M26.4,12l1.4-1.4c1.2-1.2,1.1-3.1-0.1-4.3l-3-3c-0.6-0.6-1.3-0.9-2.2-0.9c-0.8,0-1.6,0.3-2.2,0.9L19,4.6L26.4,12z"></path> </g> <g> <path d="M28,29H4c-0.6,0-1-0.4-1-1s0.4-1,1-1h24c0.6,0,1,0.4,1,1S28.6,29,28,29z"></path> </g> </g></svg>`
+			  }       
+		  },
+		  table: {
+			class: Table,
+			inlineToolbar: true,
+			config: {
+			  rows: 2,
+			  cols: 3,
+			},
+		  },			
 		quiz: Quiz,
 		upload: Upload,
 		image: SimpleImage,
